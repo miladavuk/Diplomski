d171dc3b4de232d6a312ed9fe84399f500b89cb4
miladavuk <milada.vukovic@gmail.com>
2018-08-22 23:54:49 +0200

    Dodata klasa SecurityWebApplicationInitializer koja omogucava inicijalizaciju web aplikacije koja koristi Spring Security

---
+++ milada-vukovic-diplomski/src/main/java/com/miladavukovic/diplomski/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.miladavukovic.diplomski.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+	//this is all that is needed for this class
+
+}
