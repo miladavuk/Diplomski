6a6d183cf54dd2dbdabdabfdf7757e7ec8faf612
miladavuk <milada.vukovic@gmail.com>
2018-08-29 00:46:21 +0200

    U klasi SecurityConfig podesena autentifikacija korisnika uz pomoc jdbc-a

--- milada-vukovic-diplomski/src/main/java/com/miladavukovic/diplomski/config/SecurityConfig.java
+++ milada-vukovic-diplomski/src/main/java/com/miladavukovic/diplomski/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.miladavukovic.diplomski.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,16 +15,20 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	// add a reference to our security data source
+	
+		@Autowired
+		private DataSource securityDataSource;
+		
+		
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		// users that can log in, this is temporary, just for testing
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("milada").password("test123").roles("USER"));
-	}
+			// use jdbc authentication 
+			
+			auth.jdbcAuthentication().dataSource(securityDataSource);
+			
+		}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
